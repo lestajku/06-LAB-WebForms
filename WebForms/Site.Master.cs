b1118c8d66de0c82a3d22f356e0f477f760a3f94
lestajku <lestajku@vsite.hr>
2026-01-21 22:29:58 +0100

    6.Labos - riješen

--- WebForms/Site.Master.cs
+++ WebForms/Site.Master.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Data;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
 
 namespace WebForms
 {
